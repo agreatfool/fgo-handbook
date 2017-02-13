fcb10ad68342570ec73034b4f2fb3a486809e989
Jonathan Dai <nightghost5078@gmail.com>
2017-02-13 19:26:07 +0800

    Rename files.

--- src/mobile/app/App.tsx
+++ src/mobile/app/App.tsx
@@ -5,8 +5,8 @@ import {createStore, applyMiddleware, combineReducers} from "redux";
 import {Provider} from "react-redux";
 
 import Reducers from "../app/Reducers";
-import PageOne from "../scene/page_one/app";
-import PageTwo from "../scene/page_two/app";
+import PageOne from "../scene/page_one/App";
+import PageTwo from "../scene/page_two/App";
 
 const store = createStore(
     combineReducers(Reducers),
