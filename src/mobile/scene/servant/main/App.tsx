f199ad798908f110b8b4fefb9a32461f380d0c2b
Jonathan Dai <nightghost5078@gmail.com>
2017-02-24 15:31:08 +0800

    Remove unnecessary codes & move scene position.

--- src/mobile/scene/servant/main/App.tsx
+++ src/mobile/scene/servant/main/App.tsx
@@ -2,14 +2,18 @@ import React, {Component} from "react";
 import {View, Text} from "react-native";
 import {Actions} from "react-native-router-flux";
 
-import injectIntoComponent from "../../../lib/react/Connect";
+import injectIntoComponent from "../../../../lib/react/Connect";
+
+import {App as Row} from "../../../component/row_cells/App";
+import {App as Cell} from "../../../component/cell/App";
 
 class Servant extends Component<any, any> {
     render() {
         //noinspection TypeScriptUnresolvedVariable
         return (
             <View style={{marginTop: 0}}>
-                <Text>Servant</Text>
+                <Row/>
+                <Row/>
             </View>
         );
     }
