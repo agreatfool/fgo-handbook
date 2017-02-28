bd5fd7a63a155600dcbe7c2f5dc6d45ec01b4248
Jonathan Dai <nightghost5078@gmail.com>
2017-02-28 10:58:43 +0800

    Move component content into App level, since it cannot be reused.

--- src/mobile/scene/servant/main/App.tsx
+++ src/mobile/scene/servant/main/App.tsx
@@ -1,19 +1,71 @@
 import React, {Component} from "react";
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet, Image, FlexDirection, ImageResizeMode} from "react-native";
 import {Actions} from "react-native-router-flux";
 
 import injectIntoComponent from "../../../../lib/react/Connect";
 
-import {App as Row} from "../../../component/row_cells/App";
-import {App as Cell} from "../../../component/cell/App";
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row" as FlexDirection,
+        marginTop: 5,
+        marginBottom: 5
+    },
+    cell: {
+        flex: 1,
+        width: 70,
+        height: 70,
+        marginLeft: 5,
+        marginRight: 5,
+        borderWidth: 1,
+        borderStyle: "solid" as any,
+        borderColor: "black",
+    },
+    image: {
+        flex: 1,
+        width: 70,
+        resizeMode: "contain" as ImageResizeMode
+    }
+});
 
 class Servant extends Component<any, any> {
     render() {
         //noinspection TypeScriptUnresolvedVariable
         return (
             <View style={{marginTop: 0}}>
-                <Row/>
-                <Row/>
+                <View style={styles.row}>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                </View>
+                <View style={styles.row}>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                    <View style={styles.cell}>
+                        <Image style={styles.image} source={require("../../../../../database/0.0.1/images/face/100100.png")}/>
+                    </View>
+                </View>
             </View>
         );
     }
