b626f6a6e3bd9ffbcfb8bbe536cb2de27d97becc
Jonathan Dai <nightghost5078@gmail.com>
2017-05-22 17:47:50 +0800

    Goal list page done.

--- src/mobile/scene/goal/list/App.tsx
+++ src/mobile/scene/goal/list/App.tsx
@@ -1,31 +1,19 @@
 import React, {Component} from "react";
-import {TouchableOpacity, View, Text, Alert, StyleSheet} from "react-native";
-import * as Renderer from "../../../view/View";
-// import {
-//     ToolBoxWrapper,
-//     TabScene,
-//     TabPageScroll,
-//     Table,
-//     ResImageWithElement,
-//     ResImageWithElementPlaceholder
-// } from "../../../view/View";
+import {StyleSheet, Text, View} from "react-native";
+import {ColCard, ColCardWithRightButton, ColCardWrapper, ColR, GridLine, TextCentering} from "../../../view/View";
 import injectIntoComponent from "../../../../lib/react/Connect";
 import MstLoader from "../../../lib/model/MstLoader";
-import {MstGoal, Goal, defaultCurrentGoal} from "../../../lib/model/MstGoal";
+import {defaultCurrentGoal, Goal, MstGoal} from "../../../lib/model/MstGoal";
 import * as State from "./State";
 import * as Action from "./Action";
 import MstUtil from "../../../lib/utility/MstUtil";
 import * as MstService from "../../../service/MstService";
 import {MstSvt} from "../../../../model/master/Master";
 import BaseContainer from "../../../../lib/container/base/BaseContainer";
-import {MstSkillContainer, MstSvtSkillContainer, MstCombineSkillContainer} from "../../../../model/impl/MstContainer";
+import {MstCombineSkillContainer, MstSkillContainer, MstSvtSkillContainer} from "../../../../model/impl/MstContainer";
 import {Actions} from "react-native-router-flux";
-import {Body, Button, Col, Container, Content, Header, Icon, Left, Right, Toast, Row, Thumbnail, Title, Picker, Item} from "native-base";
+import {Body, Button, Container, Content, Header, Icon, Left, Picker, Right, Row, Title, Toast} from "native-base";
 import * as Styles from "../../../view/Styles";
-import {
-    ColCard, ColR, ColCentering, GridColCardWrapper, GridLine, RowCentering, TextCentering, ColCardWithRightButton,
-    ColCardWrapper
-} from "../../../view/View";
 import {AppFooterTab, AppFooterTabIndex} from "../../../component/app_footer_tab/App";
 
 export * from "./State";
@@ -82,110 +70,8 @@ class GoalList extends Component<State.Props, any> {
         });
     }
 
-    // genItemCountStr(count: number) {
-    //     return `x${count}`;
-    // }
-
-    // prepareGoalData(goal: Goal) {
-    //     let column = Renderer.buildColumnData(`Goal ${goal.name}`, []);
-    //
-    //     const CELL_COUNT = 5;
-    //
-    //     let cells = [];
-    //     goal.items.forEach((item: GoalItem) => {
-    //         cells.push(
-    //             <ResImageWithElement
-    //                 appVer={this._appVer}
-    //                 type="item"
-    //                 id={item.itemId}
-    //                 size="small"
-    //                 width={77}
-    //                 text={this.genItemCountStr(item.count)}
-    //             />
-    //         );
-    //     });
-    //
-    //     let rows = [cells];
-    //     if (cells.length > CELL_COUNT) {
-    //         rows = [];
-    //         for (let index = 0, loop = cells.length; index < loop; index += CELL_COUNT) {
-    //             rows.push(cells.slice(index, index + CELL_COUNT));
-    //         }
-    //     }
-    //
-    //     if (rows[rows.length - 1].length < CELL_COUNT) {
-    //         let lastRow = rows.pop();
-    //         let appendCount = CELL_COUNT - lastRow.length;
-    //         for (let loop = 0; loop < appendCount; loop++) {
-    //             lastRow.push(<ResImageWithElementPlaceholder width={77} />);
-    //         }
-    //         rows.push(lastRow);
-    //     }
-    //     rows.push([
-    //         <TouchableOpacity>
-    //             <Text>Button</Text>
-    //         </TouchableOpacity>
-    //     ]);
-    //
-    //     column.rows = rows;
-    //
-    //     return [column];
-    // }
-
-    prepareData(goals: Array<Goal>) {
-        // let props = this.props as State.Props;
-        // let column = Renderer.buildColumnData(`Goal List`, []);
-        //
-        // column.rows.push([
-        //     "名字",
-        //     "数量",
-        //     <ActionButtonText>编辑</ActionButtonText>,
-        //     <ActionButtonText>扩展</ActionButtonText>,
-        //     <ActionButtonText>比较</ActionButtonText>,
-        //     <ActionButtonText>删除</ActionButtonText>,
-        // ]);
-        //
-        // goals.forEach((goal: Goal) => {
-        //     column.rows.push([
-        //         goal.name,
-        //         goal.servants.length,
-        //         <ActionButton action="edit" goal={goal} injectedActions={props.actions} />,
-        //         <ActionButton action="extend" goal={goal} injectedActions={props.actions} />,
-        //         <ActionButton action="compare" goal={goal} injectedActions={props.actions} />,
-        //         <ActionButton action="delete" goal={goal} injectedActions={props.actions} />,
-        //     ]);
-        // });
-        //
-        // return [[column]];
-    }
-
-    render1() {
-        // let table = <View />;
-        // let goals: Array<Goal> = (this.props as State.Props).SceneGoal.goals;
-        // if (goals && goals.length !== 0) {
-        //     table = <Table pageName="GoalList" data={this.prepareData(goals)}/>;
-        // }
-        //
-        // //noinspection TypeScriptUnresolvedVariable,TypeScriptUnresolvedFunction
-        // return (
-        //     <TabScene>
-        //         <ToolBoxWrapper
-        //             pageName="GoalList"
-        //             buttons={[
-        //                 {content: "新建目标", onPress: () => (Actions as any).goal_edit({
-        //                     mode: "add", isCurrent: false, goalId: undefined
-        //                 })},
-        //                 {content: "编辑现状", onPress: () => (Actions as any).goal_edit({
-        //                     mode: "edit", isCurrent: true, goalId: undefined
-        //                 })},
-        //                 {content: "经验计算器", onPress: () => (Actions as any).goal_exp()},
-        //             ]}
-        //         />
-        //         <TabPageScroll>
-        //             {table}
-        //         </TabPageScroll>
-        //     </TabScene>
-        // );
+    genItemCountStr(count: number) {
+        return `x${count}`;
     }
 
     renderGoalsPicker(header: string, goals: Array<Goal>, selectedValue: string, onChange: (value: string) => void) {
@@ -193,10 +79,11 @@ class GoalList extends Component<State.Props, any> {
         let state = props.SceneGoal;
 
         let pickerGoalItems = [
-            <Picker.Item key={`PickerItem_${header}_Current`} label={state.current.name} value={state.current.id} />
+            <Picker.Item key={`PickerItem_${header}_Current`} label={state.current.name} value={state.current.id}/>
         ];
         goals.forEach((goal: Goal, index) => {
-            pickerGoalItems.push(<Picker.Item key={`PickerItem_${header}_${index}`} label={goal.name} value={goal.id} />);
+            pickerGoalItems.push(<Picker.Item key={`PickerItem_${header}_${index}`} label={goal.name}
+                                              value={goal.id}/>);
         });
 
         return (
@@ -279,15 +166,60 @@ class GoalList extends Component<State.Props, any> {
         let props = this.props as State.Props;
         let state = props.SceneGoal;
 
-        let result = <View />;
         let goals: Array<Goal> = state.goals;
         if (!goals || goals.length === 0) {
-            return result;
+            return <View />;
         }
 
-        // FIXME 等有数据了再回头做这块
+        let goalList = [];
+        goals.forEach((goal: Goal, index) => {
+            //noinspection TypeScriptUnresolvedFunction
+            goalList.push(
+                <Row key={`Goal_${index}`}>
+                    <ColCardWrapper>
+                        <Row>
+                            <ColR size={1.2} style={Styles.Common.VerticalCentering}>
+                                <Text>{goal.name}</Text>
+                            </ColR>
+                            <ColR size={.8} style={Styles.Common.VerticalCentering}>
+                                <Text>{`从者 ${this.genItemCountStr(goal.servants.length)}`}</Text>
+                            </ColR>
+                            <ColR size={.9}>
+                                <Button outline small info block bordered
+                                        style={{marginLeft: 5}}
+                                        onPress={() => (Actions as any).goal_edit({
+                                            mode: "edit", isCurrent: false, goalId: goal.id
+                                        })}>
+                                    <Text>编辑</Text>
+                                </Button>
+                            </ColR>
+                            <ColR size={.9}>
+                                <Button outline small success block bordered
+                                        style={{marginLeft: 5}}
+                                        onPress={() => (Actions as any).goal_edit({
+                                            mode: "extend", isCurrent: false, goalId: goal.id
+                                        })}>
+                                    <Text>扩展</Text>
+                                </Button>
+                            </ColR>
+                            <ColR size={.9}>
+                                <Button outline small danger block bordered
+                                        style={{marginLeft: 5}}
+                                        onPress={() => props.actions.deleteGoal(goal.id)}>
+                                    <Text>删除</Text>
+                                </Button>
+                            </ColR>
+                        </Row>
+                    </ColCardWrapper>
+                </Row>
+            );
+        });
 
-        return result;
+        return (
+            <GridLine>
+                {goalList}
+            </GridLine>
+        );
     }
 
     render() {
@@ -313,7 +245,7 @@ class GoalList extends Component<State.Props, any> {
                                 button="Go"
                                 onPress={() => (Actions as any).goal_edit({
                                     mode: "edit", isCurrent: true, goalId: undefined
-                                })} />
+                                })}/>
                         </GridLine>
                         <GridLine>
                             <ColCardWithRightButton
@@ -321,11 +253,11 @@ class GoalList extends Component<State.Props, any> {
                                 button="Go"
                                 onPress={() => (Actions as any).goal_edit({
                                     mode: "add", isCurrent: false, goalId: undefined
-                                })} />
+                                })}/>
                         </GridLine>
                         {this.renderCompareButton()}
                         <GridLine key="GoalServantList">
-                            <ColCard items={["进度列表"]} backgroundColor="#CDE1F9" />
+                            <ColCard items={["进度列表"]} backgroundColor="#CDE1F9"/>
                         </GridLine>
                         <Container>
                             <Content>
@@ -337,75 +269,6 @@ class GoalList extends Component<State.Props, any> {
                 <AppFooterTab activeIndex={AppFooterTabIndex.Progress}/>
             </Container>
         );
-    }
-}
-
-interface ActionButtonProps extends Renderer.Props {
-    action: string;
-    goal: Goal;
-    injectedActions: any;
-}
-
-class ActionButton extends Component<ActionButtonProps, any> {
-    render1() {
-        // let props = this.props as ActionButtonProps;
-        // let text = "";
-        // let onPress = () => {};
-        //
-        // switch (props.action) {
-        //     case "compare":
-        //         text = "比对";
-        //         //noinspection TypeScriptUnresolvedFunction
-        //         onPress = () => (Actions as any).goal_compare({
-        //             goalId: props.goal.id
-        //         });
-        //         break;
-        //     case "edit":
-        //         text = "编辑";
-        //         //noinspection TypeScriptUnresolvedFunction
-        //         onPress = () => (Actions as any).goal_edit({
-        //             mode: "edit", isCurrent: false, goalId: props.goal.id
-        //         });
-        //         break;
-        //     case "delete":
-        //         text = "删除";
-        //         onPress = () => {
-        //             Alert.alert("确认删除该目标吗？", null, [
-        //                 {text: "取消"},
-        //                 {text: "确定", onPress: () => props.injectedActions.deleteGoal(props.goal.id)},
-        //             ]);
-        //         };
-        //         break;
-        //     case "extend":
-        //         text = "扩展";
-        //         //noinspection TypeScriptUnresolvedFunction
-        //         onPress = () => (Actions as any).goal_edit({
-        //             mode: "extend", isCurrent: false, goalId: props.goal.id
-        //         });
-        //         break;
-        //     default:
-        //         onPress = () => console.error("Wrong action of ActionButton on page GoalList", props.action);
-        //         break;
-        // }
-        //
-        // return (
-        //     <TouchableOpacity
-        //         style={[
-        //             Styles.Common.verticalCentering,
-        //             {width: 50, height: Renderer.TABLE_CONTENT_HEIGHT_DEFAULT},
-        //             {backgroundColor: "yellow"}
-        //         ]}
-        //         onPress={onPress}
-        //     >
-        //         <Text style={[
-        //             Styles.Common.textCenter,
-        //         ]}>{text}</Text>
-        //     </TouchableOpacity>
-        // );
-    }
-
-    render() {
-        return <View/>;
     }
 }
 
