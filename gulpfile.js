b8fcffb61397da37b0daa6080ebc8fe555eb340b
Jonathan Dai <nightghost5078@gmail.com>
2017-09-01 17:16:40 +0800

    Add Server side template for ImagePool class.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ const merge = require('merge2');
 
 const rnSource = ['src/**/*.ts', 'src/**/*.tsx', '!src/{server,server/**}'];
 const serverSource = ['src/**/*.ts', '!src/{mobile,mobile/**}'];
+const serverTemplateSource = 'src/server/template/**/*.hbs';
 const phantomSource = 'src/server/phantom/exec.js';
+
 const tsRnProject = ts.createProject('tsconfig.json', {
   declaration: false
 });
@@ -31,13 +33,19 @@ gulp.task('tsServer', function () {
   ]);
 });
 
+gulp.task('server-copy', function () {
+  gulp.src(serverTemplateSource)
+    .pipe(gulp.dest('server_build/server/template'));
+});
+
 gulp.task('phantom-copy', function () {
   gulp.src(phantomSource)
     .pipe(gulp.dest('server_build/server/phantom'));
 });
 
-gulp.task('watch', ['tsRn', 'tsServer', 'phantom-copy'], function () {
+gulp.task('watch', ['tsRn', 'tsServer', 'server-copy', 'phantom-copy'], function () {
   gulp.watch(rnSource, ['tsRn']);
   gulp.watch(serverSource, ['tsServer']);
+  gulp.watch(serverTemplateSource, ['server-copy']);
   gulp.watch(phantomSource, ['phantom-copy']);
 });
