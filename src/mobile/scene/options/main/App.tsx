8136847414c04cd5bd80989ca7fa1221701b58e6
Jonathan Dai <nightghost5078@gmail.com>
2017-12-14 14:52:01 +0800

    Fix options back button.

--- src/mobile/scene/options/main/App.tsx
+++ src/mobile/scene/options/main/App.tsx
@@ -1,14 +1,50 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {View} from "react-native";
+import {
+    Body,
+    Button,
+    Card,
+    CardItem,
+    Col,
+    Container,
+    Content,
+    Grid,
+    Header,
+    Icon,
+    Left,
+    Picker,
+    Right,
+    Row,
+    Title,
+    Toast
+} from "native-base";
+import * as Styles from "../../../view/Styles";
 
 import injectIntoComponent from "../../../../lib/react/Connect";
+import {ContainerWhite} from "../../../view/View";
+import {AppFooterTab, AppFooterTabIndex} from "../../../component/app_footer_tab/App";
 
 class Options extends Component<any, any> {
     render() {
         return (
-            <View style={{marginTop: 0}}>
-                <Text>Options</Text>
-            </View>
+            <ContainerWhite>
+                <Header>
+                    <Left>
+                        <Button transparent onPress={() => this.props.navigation.goBack(null)}>
+                            <Icon name="arrow-back"/>
+                        </Button>
+                    </Left>
+                    <Body>
+                    <Title>Options</Title>
+                    </Body>
+                    <Right/>
+                </Header>
+                <Content>
+                    <View style={Styles.Box.Wrapper}>
+                    </View>
+                </Content>
+                <AppFooterTab activeIndex={AppFooterTabIndex.Option} navigation={this.props.navigation}/>
+            </ContainerWhite>
         );
     }
 }
